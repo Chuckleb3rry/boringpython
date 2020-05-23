522ba4b39bb9cdf6a882dd4637d3dff4689bac6f
Chuckleb3rry <48499321+Chuckleb3rry@users.noreply.github.com>
2020-05-23 14:51:31 -0700

    Add files via upload

---
+++ Collatz.py
@@ -0,0 +1,17 @@
+## This program performs the Collatz function on a
+## positive integer input by the user
+
+print("Please enter a postive integer")
+Number = int(input())
+##need error correction for non integers - use try and except
+
+if Number ==1
+
+
+##added comment forgithub test
+
+while number != 1
+
+
+
+print(Number)
